1e98a7e43dcf74bd0f3b6456af46f92285296592
luangitdev <luaninveste@gmail.com>
2023-10-21 01:51:06 -0300

    Alteração no master.sh

--- master.sh
+++ master.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
-sudo docker swarm init --advertise-addr=10.10.10.100
-sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+
+# Configurações
+ADVERTISE_ADDR="10.10.10.100"
+OUTPUT_FILE="/vagrant/worker.sh"
+
+# Função para inicializar o Docker Swarm
+init_docker_swarm() {
+    echo "Iniciando o Docker Swarm..."
+    if sudo docker swarm init --advertise-addr="$ADVERTISE_ADDR"; then
+        echo "Docker Swarm iniciado com sucesso."
+    else
+        echo "Erro ao iniciar o Docker Swarm."
+        exit 1
+    fi
+}
+
+# Função para gerar o script de junção do worker
+generate_worker_join_script() {
+    echo "Gerando script de junção do worker..."
+    if sudo docker swarm join-token worker | grep docker > "$OUTPUT_FILE"; then
+        echo "Script de junção do worker gerado em $OUTPUT_FILE."
+    else
+        echo "Erro ao gerar o script de junção do worker."
+        exit 1
+    fi
+}
+
+# Função principal
+main() {
+    init_docker_swarm
+    generate_worker_join_script
+}
+
+main
